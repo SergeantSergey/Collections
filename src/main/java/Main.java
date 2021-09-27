2e561f581d873106d8b7e03b3e8b9619bedd4203
sergey.startsev@x5.ru <56startcev5611RUS>
2021-09-27 23:23:46 +0300

    Lesson 5: Add search and remove

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -24,9 +24,23 @@ public class Main {
         for (int i = 0; i < linkedList.size(); i++) {
             System.out.println(linkedList.get(i));
         }
+        System.out.println("---");
+        System.out.println(linkedList.getFirst());
+        System.out.println(linkedList.getLast());
+        System.out.println("---");
+        linkedList.remove(a);
+        System.out.println(linkedList.getFirst());
+        System.out.println(linkedList.getLast());
+        System.out.println("--- Final Linked List");
 
         for (int i = 0; i < arrayList.size(); i++) {
             System.out.println(arrayList.get(i));
         }
+        System.out.println("---");
+        System.out.println(arrayList.size());
+        System.out.println("---");
+        arrayList.remove(a);
+        System.out.println(arrayList.size());
+        System.out.println("--- Final Array List");
     }
 }
